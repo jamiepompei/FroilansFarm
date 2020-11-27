bf677a12f8771e5dd1b8441affb8b54d32477227
shubham <shubham@zipcodes-MBP-5.fios-router.home>
2020-11-27 17:28:30 -0500

    Edits in the Silo

--- src/main/java/com/zipcodewilmington/froilansfarm/buildings/Silo.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/buildings/Silo.java
@@ -10,39 +10,25 @@ import java.util.Arrays;
 
 public class Silo {
 
-    ArrayList<Corn> arrListCorn;
-    ArrayList<Tomato> arrListTomato;
-    ArrayList<Squash> arrListSquash;
-    ArrayList<Egg> arrListEgg;
-
-    public Silo() {
-
-        Egg[] eggs= {new Egg(), new Egg(), new Egg(), new Egg(), new Egg(), new Egg(), new Egg(), new Egg(), new Egg(), new Egg()};
-        Corn[] corns = {new Corn(),new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(),
-                new Corn(),new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(),
-                new Corn(),new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn(), new Corn()};
-        Tomato[] tomatoes = {new Tomato(), new Tomato(), new Tomato(), new Tomato(), new Tomato()};
+    public static ArrayList<Corn> arrListCorn = new ArrayList<Corn>();
+    public static ArrayList<Tomato> arrListTomato = new ArrayList<Tomato>();
+    public static ArrayList<Squash> arrListSquash = new ArrayList<Squash>();
+    public static ArrayList<Egg> arrListEgg = new ArrayList<Egg>();
 
 
-        arrListCorn = new ArrayList<Corn>(Arrays.asList(corns));
-        arrListTomato = new ArrayList<Tomato>(Arrays.asList(tomatoes));
-        arrListEgg = new ArrayList<Egg>(Arrays.asList(eggs));
-        arrListSquash = new ArrayList<Squash>();
-
-    }
-    public ArrayList<Corn> getArrListCorn() {
+    public static ArrayList<Corn> getArrListCorn() {
         return arrListCorn;
     }
 
-    public ArrayList<Tomato> getArrListTomato() {
+    public static ArrayList<Tomato> getArrListTomato() {
         return arrListTomato;
     }
 
-    public ArrayList<Squash> getArrListSquash() {
+    public static ArrayList<Squash> getArrListSquash() {
         return arrListSquash;
     }
 
-    public ArrayList<Egg> getArrListEgg() {
+    public static ArrayList<Egg> getArrListEgg() {
         return arrListEgg;
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/vehicle/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/vehicle/Tractor.java
@@ -1,19 +1,34 @@
 package com.zipcodewilmington.froilansfarm.vehicle;
 
+import com.zipcodewilmington.froilansfarm.field.Crop;
+import com.zipcodewilmington.froilansfarm.field.Field;
+
 public class Tractor extends FarmVehicle{
+    Boolean isBeingRidden;
+    Field field;
+
     public String makeNoise() {
-        return null;
+        return "Obnoxious noise.. crunch munch!!";
     }
 
     public Boolean getIsBeingRidden() {
-        return null;
+        return isBeingRidden;
     }
 
     public void setIsBeingRidden(Boolean isBeingRidden) {
 
     }
+    public void harvest(Integer rowNum) {
 
-    //
+        for (Crop c : field.getRows().get(rowNum - 1).getListOfCrop()
+        ) {
+            if (c.getFertilized()) {
+                c.setPlanted(false);
+                c.setHarvested(true);
+            }
+        }
+    }
+
 
 
 }
